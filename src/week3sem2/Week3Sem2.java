0338d42ee55e36fbee9767af2d912d3dfd86738f
kauveeri <kauveeri@sheridanc.on.ca>
2022-01-24 09:04:37 -0500

    version 1

---
+++ src/week3sem2/Week3Sem2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3sem2;
+
+/**
+ *
+ * @author Jassu01
+ */
+public class Week3Sem2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Learning github");
+    }
+    
+}
